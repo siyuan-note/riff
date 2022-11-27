77ce5f6ba5bb07740e03c9d13d8c7c8429897ace
Liang Ding <d@b3log.org>
2022-11-28 00:21:12 +0800

    :tada: Init commit

--- go.mod
+++ go.mod
@@ -3,16 +3,14 @@ module github.com/siyuan-note/riff
 go 1.19
 
 require (
-	github.com/88250/gulu v1.2.3-0.20221117052724-cd06804db798
-	github.com/json-iterator/go v1.1.12
 	github.com/siyuan-note/logging v0.0.0-20221031125421-9b7234d79d8a
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 )
 
 require (
+	github.com/88250/gulu v1.2.3-0.20221117052724-cd06804db798 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/getsentry/sentry-go v0.15.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
--- store/store_test.go
+++ store/store_test.go
@@ -16,108 +16,109 @@
 
 package store
 
-import (
-	"os"
-	"testing"
-	"time"
-
-	"github.com/88250/gulu"
-	jsoniter "github.com/json-iterator/go"
-	"github.com/siyuan-note/riff/fsrs"
-	"github.com/vmihailenco/msgpack/v5"
-)
-
-func TestJSONMsgPack(t *testing.T) {
-	os.MkdirAll("testdata", 0755)
-	defer os.RemoveAll("testdata")
-
-	const cardsJSON = "testdata/cards.json"
-	const cardsMsgPack = "testdata/cards.msgpack"
-
-	p := fsrs.DefaultParam()
-	start := time.Now()
-	repeatTime := start
-	var cards []*fsrs.Card
-	for i := 0; i < 100000; i++ {
-		card := fsrs.NewCard()
-		cards = append(cards, &card)
-
-		for j := 0; j < 10; j++ {
-			schedulingCards := p.Repeat(&card, repeatTime)
-			card = schedulingCards.Hard
-			repeatTime = card.Due
-		}
-		repeatTime = start
-	}
-	cardsLen := len(cards)
-	t.Logf("cards len [%d]", cardsLen)
-
-	now := time.Now()
-	data, err := gulu.JSON.MarshalJSON(cards)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = os.WriteFile(cardsJSON, data, 0644); nil != err {
-		t.Fatal(err)
-	}
-	t.Logf("save by [json] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-
-	now = time.Now()
-	data, err = jsoniter.Marshal(cards)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = os.WriteFile(cardsJSON, data, 0644); nil != err {
-		t.Fatal(err)
-	}
-	t.Logf("save by [jsoniter] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-
-	now = time.Now()
-	data, err = msgpack.Marshal(cards)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = os.WriteFile(cardsMsgPack, data, 0644); nil != err {
-		t.Fatal(err)
-	}
-	t.Logf("save by [masgpack] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-
-	now = time.Now()
-	data, err = os.ReadFile(cardsJSON)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = gulu.JSON.UnmarshalJSON(data, &cards); nil != err {
-		t.Fatal(err)
-	}
-	if cardsLen != len(cards) {
-		t.Fatal("cards len not equal")
-	}
-	t.Logf("load by [json] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-
-	now = time.Now()
-	data, err = os.ReadFile(cardsJSON)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = jsoniter.Unmarshal(data, &cards); nil != err {
-		t.Fatal(err)
-	}
-	if cardsLen != len(cards) {
-		t.Fatal("cards len not equal")
-	}
-	t.Logf("load by [jsoniter] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-
-	now = time.Now()
-	data, err = os.ReadFile(cardsMsgPack)
-	if nil != err {
-		t.Fatal(err)
-	}
-	if err = msgpack.Unmarshal(data, &cards); nil != err {
-		t.Fatal(err)
-	}
-	if cardsLen != len(cards) {
-		t.Fatal("cards len not equal")
-	}
-	t.Logf("load by [masgpack] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
-}
+//
+//import (
+//	"os"
+//	"testing"
+//	"time"
+//
+//	"github.com/88250/gulu"
+//	jsoniter "github.com/json-iterator/go"
+//	"github.com/siyuan-note/riff/fsrs"
+//	"github.com/vmihailenco/msgpack/v5"
+//)
+//
+//func TestJSONMsgPack(t *testing.T) {
+//	os.MkdirAll("testdata", 0755)
+//	defer os.RemoveAll("testdata")
+//
+//	const cardsJSON = "testdata/cards.json"
+//	const cardsMsgPack = "testdata/cards.msgpack"
+//
+//	p := fsrs.DefaultParam()
+//	start := time.Now()
+//	repeatTime := start
+//	var cards []*fsrs.Card
+//	for i := 0; i < 100000; i++ {
+//		card := fsrs.NewCard()
+//		cards = append(cards, &card)
+//
+//		for j := 0; j < 10; j++ {
+//			schedulingCards := p.Repeat(&card, repeatTime)
+//			card = schedulingCards.Hard
+//			repeatTime = card.Due
+//		}
+//		repeatTime = start
+//	}
+//	cardsLen := len(cards)
+//	t.Logf("cards len [%d]", cardsLen)
+//
+//	now := time.Now()
+//	data, err := gulu.JSON.MarshalJSON(cards)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = os.WriteFile(cardsJSON, data, 0644); nil != err {
+//		t.Fatal(err)
+//	}
+//	t.Logf("save by [json] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//
+//	now = time.Now()
+//	data, err = jsoniter.Marshal(cards)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = os.WriteFile(cardsJSON, data, 0644); nil != err {
+//		t.Fatal(err)
+//	}
+//	t.Logf("save by [jsoniter] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//
+//	now = time.Now()
+//	data, err = msgpack.Marshal(cards)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = os.WriteFile(cardsMsgPack, data, 0644); nil != err {
+//		t.Fatal(err)
+//	}
+//	t.Logf("save by [masgpack] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//
+//	now = time.Now()
+//	data, err = os.ReadFile(cardsJSON)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = gulu.JSON.UnmarshalJSON(data, &cards); nil != err {
+//		t.Fatal(err)
+//	}
+//	if cardsLen != len(cards) {
+//		t.Fatal("cards len not equal")
+//	}
+//	t.Logf("load by [json] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//
+//	now = time.Now()
+//	data, err = os.ReadFile(cardsJSON)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = jsoniter.Unmarshal(data, &cards); nil != err {
+//		t.Fatal(err)
+//	}
+//	if cardsLen != len(cards) {
+//		t.Fatal("cards len not equal")
+//	}
+//	t.Logf("load by [jsoniter] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//
+//	now = time.Now()
+//	data, err = os.ReadFile(cardsMsgPack)
+//	if nil != err {
+//		t.Fatal(err)
+//	}
+//	if err = msgpack.Unmarshal(data, &cards); nil != err {
+//		t.Fatal(err)
+//	}
+//	if cardsLen != len(cards) {
+//		t.Fatal("cards len not equal")
+//	}
+//	t.Logf("load by [masgpack] elapsed [%dms], size [%.2fMB]", time.Since(now).Milliseconds(), float64(len(data))/1024/1024)
+//}
