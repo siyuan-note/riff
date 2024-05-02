13f9b0dabfe4c1a30980d714a695c51dd2a500d4
zxhd <zxhd863943427@outlook.com>
2024-05-02 11:06:20 +0800

    实现基本字段要求

--- card.go
+++ card.go
@@ -23,14 +23,32 @@ type Card interface {
 	// ID 返回闪卡 ID。
 	ID() string
 
-	// BlockID 返回闪卡关联的内容块 ID。
-	BlockID() string
-
-	// CardSourceID 返回关联的cardsource ID，对于默认卡直接返回自身闪卡ID
+	// CardSourceID 返回关联的cardsource ID
 	CardSourceID() string
 
-	// CardType 返回闪卡的类型，默认的卡片直接返回default
-	CardType() string
+	// GetGroup 返回当前卡片的Group
+	GetGroup() string
+
+	// SetGroup 设置当前卡片的Group
+	SetGroup(newGroup string)
+
+	// GetTag 返回当前卡片的Tag
+	GetTag() string
+
+	// SetTag 设置当前卡片的Tag
+	SetTag(newTag string)
+
+	// GetSuspend 返回当前卡片的暂停状态
+	GetSuspend() bool
+
+	// SwtichSuspend 切换当前卡片的暂停状态
+	SwtichSuspend()
+
+	// GetContext 返回当前卡片的Context
+	GetContext() map[string]string
+
+	// SetContext 使用 value 设置当前卡片的 key
+	SetContext(key string, value string)
 
 	// NextDues 返回每种评分对应的下次到期时间。
 	NextDues() map[Rating]time.Time
@@ -65,9 +83,13 @@ type Card interface {
 
 // BaseCard 描述了基础的闪卡实现。
 type BaseCard struct {
-	CID   string
-	BID   string
-	NDues map[Rating]time.Time
+	CID     string
+	SID     string
+	Group   string
+	Tag     string
+	Suspend bool
+	Context map[string]string
+	NDues   map[Rating]time.Time
 }
 
 func (card *BaseCard) NextDues() map[Rating]time.Time {
@@ -82,14 +104,38 @@ func (card *BaseCard) ID() string {
 	return card.CID
 }
 
-func (card *BaseCard) BlockID() string {
-	return card.BID
-}
-
 func (card *BaseCard) CardSourceID() string {
-	return card.BID
+	return card.SID
 }
 
-func (card *BaseCard) CardType() string {
-	return "default"
+func (card *BaseCard) GetGroup() string {
+	return card.Group
+}
+
+func (card *BaseCard) SetGroup(newGroup string) {
+	card.Group = newGroup
+}
+
+func (card *BaseCard) GetTag() string {
+	return card.Tag
+}
+
+func (card *BaseCard) SetTag(newTag string) {
+	card.Tag = newTag
+}
+
+func (card *BaseCard) GetSuspend() bool {
+	return card.Suspend
+}
+
+func (card *BaseCard) SwtichSuspend() {
+	card.Suspend = !card.Suspend
+}
+
+func (card *BaseCard) GetContext() map[string]string {
+	return card.Context
+}
+
+func (card *BaseCard) SetContext(key string, value string) {
+	card.Context[key] = value
 }
--- card_source.go
+++ card_source.go
@@ -8,21 +8,31 @@ type CardSource interface {
 	// 返回类型
 	CardType() string
 
-	// 返回数据
-	SourceData() map[string]string
+	// 返回卡源 Data
+	GetSourceData() string
 
-	// 更新数据
-	UpdateData(key string, value string)
+	// 更新卡源 Data
+	UpdateSourceData(NewData string)
 
 	// 返回关联的CardID list
-	CardIDs() []string
+	GetCardIDs() []string
+
+	// 更新关联的CardID list
+	SetCardIDs([]string)
+
+	// 返回卡源的 Context
+	GetContext() map[string]string
+
+	// 设置卡源的 Context
+	SetContext(key string, value string)
 }
 
 type BaseCardSource struct {
-	SID   string
-	CType string
-	CIDs  []string
-	Data  map[string]string
+	SID     string
+	CType   string
+	CIDs    []string
+	Data    string
+	Context map[string]string
 }
 
 func (source *BaseCardSource) SourceID() string {
@@ -33,13 +43,26 @@ func (source *BaseCardSource) CardType() string {
 	return source.CType
 }
 
-func (source *BaseCardSource) SourceData() map[string]string {
+func (source *BaseCardSource) GetSourceData() string {
 	return source.Data
 }
 
-func (source *BaseCardSource) UpdateData(key string, value string) {
-	source.Data[key] = value
+func (source *BaseCardSource) UpdateSourceData(NewData string) {
+	source.Data = NewData
 }
-func (source *BaseCardSource) CardIDs() []string {
+
+func (source *BaseCardSource) GetCardIDs() []string {
 	return source.CIDs
+}
+
+func (source *BaseCardSource) SetCardIDs(NewCardIDs []string) {
+	source.CIDs = NewCardIDs
+}
+
+func (source *BaseCardSource) GetContext() map[string]string {
+	return source.Context
+}
+
+func (source *BaseCardSource) SetContext(key string, value string) {
+	source.Context[key] = value
 }
