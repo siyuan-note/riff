c0509b90e35e9925e2b1dea4b2f0e7518813239f
zxhd <zxhd863943427@outlook.com>
2024-07-21 14:55:26 +0800

    finish history interface

--- card_history.go
+++ card_history.go
@@ -1,11 +1,27 @@
 package riff
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/open-spaced-repetition/go-fsrs"
+)
+
+type History interface {
+	ID() string
+
+	// 对 Impl 进行 Marshal
+	MarshalImpl()
+
+	UnmarshalImpl()
+}
 
 type BaseHistory struct {
-	HID          string
+	HID          string `xorm:"pk index"`
 	CID          string
-	Update       time.Time
+	Update       time.Time `xorm:"index"`
 	UpdateResult int
 	State        State
 	Tag          string
@@ -14,8 +30,9 @@ type BaseHistory struct {
 	Priority     float64
 	Due          time.Time
 	NDues        map[Rating]time.Time
-	algo         string
-	AlgoImpl     interface{}
+	Algo         Algo
+	AlgoImpl     interface{} `xorm:"-"`
+	AlgoImplData []uint8     `json:"-"`
 }
 
 type ReviewLog struct {
@@ -23,3 +40,71 @@ type ReviewLog struct {
 	Rate   Rating
 	Review time.Time
 }
+
+type ReviewHistory struct {
+	BaseHistory `xorm:"extends"`
+	ReviewLog   `xorm:"extends"`
+}
+
+func NewBaseHistory(c Card) (history *BaseHistory) {
+	return &BaseHistory{
+		HID:          newID(),
+		CID:          c.ID(),
+		Update:       time.Now(),
+		UpdateResult: CreateHistory, // 这里可以根据需要设置具体的更新结果
+		State:        c.GetState(),
+		Tag:          c.GetTag(),
+		Flag:         c.GetFlag(),
+		Suspend:      c.GetSuspend(),
+		Priority:     c.GetPriority(),
+		Due:          c.GetDue(),
+		NDues:        c.NextDues(), // 假设Card接口有NextDues方法
+		Algo:         c.GetAlgo(),
+		AlgoImpl:     c.Impl(),           // 假设Card接口有Impl方法
+		AlgoImplData: c.GetMarshalImpl(), // 假设Card接口有GetMarshalImpl方法
+	}
+}
+
+func (b *BaseHistory) ID() string {
+	return b.HID
+}
+
+func (b *BaseHistory) UnmarshalImpl() (err error) {
+	if b.AlgoImplData == nil || len(b.AlgoImplData) == 0 {
+		err = errors.New("dont have AlgoImplData")
+		return
+	}
+	switch b.Algo {
+	case AlgoFSRS:
+		impl := fsrs.Card{}
+		json.Unmarshal(b.AlgoImplData, &impl)
+		b.AlgoImpl = impl
+	default:
+		err = fmt.Errorf("un support Algo Type : %s", string(b.Algo))
+		return
+	}
+	return
+}
+
+func (b *BaseHistory) MarshalImpl() {
+	if len(b.AlgoImplData) != 0 && b.AlgoImpl == nil {
+		return
+	}
+	data, _ := json.Marshal(b.AlgoImpl)
+	b.AlgoImplData = data
+}
+
+func NewReviewLog(b History, rate Rating) (log *ReviewLog) {
+	log = &ReviewLog{
+		HID:    b.ID(),
+		Rate:   rate,
+		Review: time.Now(),
+	}
+	return
+}
+
+const (
+	EditUpdate    int = -2
+	CreateHistory int = -1
+	ReviewUpdate  int = 1
+)
