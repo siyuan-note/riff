de8e34781f4017a69ce8cc4ed7859a99cdc13312
zxhd <zxhd863943427@outlook.com>
2024-07-21 19:42:16 +0800

    fix bug of dup return

--- riff.go
+++ riff.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/88250/gulu"
+	"github.com/open-spaced-repetition/go-fsrs"
 
 	"github.com/siyuan-note/filelock"
 	"github.com/siyuan-note/logging"
@@ -23,6 +24,7 @@ import (
 type Riff interface {
 	Query() []map[string]interface{}
 	QueryCard() []Card
+	SetParams(algo Algo, params interface{})
 	AddDeck(deck Deck) (newDeck Deck, err error)
 	AddCardSource(cardSources []CardSource) (cardSourceList []CardSource, err error)
 	AddCard(cards []Card) (cardList []Card, err error)
@@ -39,6 +41,8 @@ type BaseRiff struct {
 	MaxRequestRetention float64
 	MinRequestRetention float64
 	lock                *sync.Mutex
+	startTime           time.Time
+	ParamsMap           map[Algo]interface{}
 }
 
 func NewBaseRiff() Riff {
@@ -47,14 +51,20 @@ func NewBaseRiff() Riff {
 	if err != nil {
 		return &BaseRiff{}
 	}
-	orm.Sync(new(BaseCard), new(BaseCardSource), new(BaseDeck), new(BaseHistory))
+	orm.Sync(new(BaseCard), new(BaseCardSource), new(BaseDeck), new(BaseHistory), new(ReviewLog))
 	riff := BaseRiff{
 		db:                  orm,
 		MaxRequestRetention: 0.95,
 		MinRequestRetention: 0.5,
 		lock:                &sync.Mutex{},
+		startTime:           time.Now(),
+		ParamsMap:           map[Algo]interface{}{},
 	}
 	return &riff
+}
+
+func (br *BaseRiff) SetParams(algo Algo, params interface{}) {
+	br.ParamsMap[algo] = params
 }
 
 func (br *BaseRiff) Query() []map[string]interface{} {
@@ -115,28 +125,6 @@ func batchCheck(table, field string, IDs []string, db xorm.Interface) (existMap 
 
 	return
 }
-
-// func (br *BaseRiff) AddCardSource(cardSources []CardSource) (cardSourceList []CardSource, err error) {
-
-// 	for _, cardSource := range cardSources {
-// 		DIDs := cardSource.GetDIDs()
-// 		for _, DID := range DIDs {
-// 			err = checkExist(br.db, &BaseDeck{
-// 				DID: DID,
-// 			})
-// 		}
-
-// 		if err != nil {
-// 			return cardSourceList, err
-// 		}
-// 		_, err = br.db.Insert(cardSource)
-// 		if err != nil {
-// 			return cardSourceList, err
-// 		}
-// 		cardSourceList = append(cardSourceList, cardSource)
-// 	}
-// 	return
-// }
 
 func (br *BaseRiff) AddCardSource(cardSources []CardSource) (cardSourceList []CardSource, err error) {
 
@@ -223,8 +211,10 @@ func (br *BaseRiff) AddCard(cards []Card) (cardList []Card, err error) {
 	}
 
 	err = session.Commit()
-	// test := make([]BaseCard, 0)
-	// br.db.Find(&test)
+	test := make([]BaseCard, 0)
+	br.db.Find(&test)
+	testDue := br.Due()
+	_ = len(testDue)
 	// fmt.Printf("add card taken %s \n", time.Since(start))
 	return
 }
@@ -300,29 +290,82 @@ func (br *BaseRiff) Save(path string) (err error) {
 	return
 }
 
+func saveHistoryData(data interface{}, suffix SaveExt, saveDirPath string, time time.Time) (err error) {
+	byteData, err := json.Marshal(data)
+	if err != nil {
+		logging.LogErrorf("marshal logs failed: %s", err)
+		return
+	}
+	yyyyMMddHHmmss := time.Format("2006-01-02-15:04:05")
+	savePath := path.Join(saveDirPath, yyyyMMddHHmmss+string(suffix))
+	err = filelock.WriteFile(savePath, byteData)
+	if err != nil {
+		logging.LogErrorf("write riff file failed: %s", err)
+		return
+	}
+	return
+}
+
 func (br *BaseRiff) SaveHistory(path string) (err error) {
 	historys := make([]BaseHistory, 0)
+	reviewLogs := make([]ReviewLog, 0)
 	err = br.db.Find(&historys)
 	if err != nil {
 		return
 	}
-	err = saveData(historys, HistoryExt, path)
+	err = saveHistoryData(historys, HistoryExt, path, br.startTime)
+	if err != nil {
+		logging.LogErrorf("write history file failed: %s", err)
+	}
+	err = saveHistoryData(reviewLogs, reviewLogExt, path, br.startTime)
+	if err != nil {
+		logging.LogErrorf("write history file failed: %s", err)
+	}
+
 	if err != nil {
 		return
 	}
 	return
 }
 
+func (br *BaseRiff) LoadHistory(historys []History, logs []ReviewLog) (err error) {
+	session := br.db.NewSession()
+	defer session.Close()
+	session.Begin()
+
+	for _, history := range historys {
+		// session.Prepare()
+		_, err = session.Insert(history)
+		if err != nil {
+			fmt.Printf("error on insert history %s \n", err)
+			continue
+		}
+	}
+	for _, log := range logs {
+		// session.Prepare()
+		_, err = session.Insert(log)
+		if err != nil {
+			fmt.Printf("error on insert log %s \n", err)
+			continue
+		}
+	}
+
+	err = session.Commit()
+	return
+}
+
 func (br *BaseRiff) Load(savePath string) (err error) {
 	// data, err := filelock.ReadFile(savePath)
 	br.lock.Lock()
+	defer br.lock.Unlock()
 	if !gulu.File.IsDir(savePath) {
 		return errors.New("no a save path")
 	}
 	totalDecks := make([]Deck, 0)
 	totalCards := make([]Card, 0)
 	totalCardSources := make([]CardSource, 0)
-	totalHistory := make([]BaseHistory, 0)
+	totalHistory := make([]History, 0)
+	totalReviewLog := make([]ReviewLog, 0)
 	filelock.Walk(savePath, func(walkPath string, info fs.FileInfo, err error) (reErr error) {
 		if info.IsDir() {
 			return
@@ -335,27 +378,37 @@ func (br *BaseRiff) Load(savePath string) (err error) {
 			decks := make([]BaseDeck, 0)
 			json.Unmarshal(data, &decks)
 			for _, deck := range decks {
-				totalDecks = append(totalDecks, &deck)
+				copy := deck
+				totalDecks = append(totalDecks, &copy)
 			}
 
 		case CardExt:
 			cards := make([]BaseCard, 0)
 			json.Unmarshal(data, &cards)
 			for _, card := range cards {
-				totalCards = append(totalCards, &card)
+				copy := card
+				totalCards = append(totalCards, &copy)
 			}
 
 		case CardSourceExt:
 			cardSources := make([]BaseCardSource, 0)
 			json.Unmarshal(data, &cardSources)
 			for _, cardSource := range cardSources {
-				totalCardSources = append(totalCardSources, &cardSource)
+				copy := cardSource
+				totalCardSources = append(totalCardSources, &copy)
 			}
 
 		case HistoryExt:
-			history := make([]BaseHistory, 0)
-			json.Unmarshal(data, &history)
-			totalHistory = append(totalHistory, history...)
+			historys := make([]BaseHistory, 0)
+			json.Unmarshal(data, &historys)
+			for _, history := range historys {
+				copy := history
+				totalHistory = append(totalHistory, &copy)
+			}
+		case reviewLogExt:
+			reviewLog := make([]ReviewLog, 0)
+			json.Unmarshal(data, &reviewLog)
+			totalReviewLog = append(totalReviewLog, reviewLog...)
 		}
 
 		return
@@ -366,26 +419,57 @@ func (br *BaseRiff) Load(savePath string) (err error) {
 
 	br.AddCardSource(totalCardSources)
 	br.AddCard(totalCards)
-	br.lock.Unlock()
+
+	go br.LoadHistory(totalHistory, totalReviewLog)
 	return
+
 }
 
 func (br *BaseRiff) Due() []Card {
 	br.lock.Lock()
+	defer br.lock.Unlock()
 	cards := make([]Card, 0)
 	baseCards := make([]BaseCard, 0)
 	now := time.Now()
 	br.db.Where("due < ?", now).Find(&baseCards)
 	for _, bc := range baseCards {
-		UnmarshalImpl(&bc)
-		cards = append(cards, &bc)
+		copy := bc
+		copy.UnmarshalImpl()
+		cards = append(cards, &copy)
+		// baseCards[index].UnmarshalImpl()
 	}
-	br.lock.Unlock()
 	return cards
 }
 
 func (br *BaseRiff) Review(card Card, rating Rating, RequestRetention float64) {
-	// 空实现
+	now := time.Now()
+
+	history := NewBaseHistory(card)
+	reviewlog := NewReviewLog(history, rating)
+	br.db.Insert(&history)
+	br.db.Insert(&reviewlog)
+
+	switch card.GetAlgo() {
+	case AlgoFSRS:
+		fsrsCard := (card.Impl()).(fsrs.Card)
+		params := br.ParamsMap[AlgoFSRS].(fsrs.Parameters)
+		params.RequestRetention = RequestRetention
+		schedulingInfo := params.Repeat(fsrsCard, now)[rating.ToFsrs()]
+		newCard := schedulingInfo.Card
+		card.SetDue(newCard.Due)
+		card.SetImpl(newCard)
+	}
+	switch rating {
+	case Again:
+		card.SetLapses(card.GetLapses() + 1)
+		card.SetReps(card.GetReps() + 1)
+	default:
+		card.SetReps(card.GetReps() + 1)
+	}
+	_, err := br.db.Where("c_i_d = ?", card.ID()).Update(card)
+	if err != nil {
+		fmt.Printf("update card err:%s\n", err)
+	}
 }
 
 func (br *BaseRiff) CountCards() int {
@@ -408,6 +492,17 @@ const (
 	Easy                    // 很容易
 )
 
+var RatingToFsrs = map[Rating]fsrs.Rating{
+	Again: fsrs.Again,
+	Hard:  fsrs.Hard,
+	Good:  fsrs.Good,
+	Easy:  fsrs.Easy,
+}
+
+func (rate Rating) ToFsrs() fsrs.Rating {
+	return RatingToFsrs[rate]
+}
+
 // Algo 描述了闪卡复习算法的名称。
 type Algo string
 
@@ -423,6 +518,7 @@ const (
 	CardSourceExt = ".cardSources"
 	DeckExt       = ".decks"
 	HistoryExt    = ".history"
+	reviewLogExt  = ".revlog"
 )
 
 // State 描述了闪卡的状态。
